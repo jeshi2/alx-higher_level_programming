07c43a2a525d05147585a43928f7fee818ebfbac
jeshi2 <jamesvean19@gmail.com>
2023-05-10 21:05:20 +0300

    19:37hrs

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 output = ""
 for i in range(122, 96, -1):
-    output += "{0}{1}".format(chr(i), chr(i - 33) if i > 97 else "")
-
+    output += "{}{}".format(chr(i), chr(i - 32) if i > 97 else "")
 print(output)
-
