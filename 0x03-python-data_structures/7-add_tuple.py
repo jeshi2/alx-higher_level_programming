83c48b11b1a2fa7e638f5248fc75bf2d074a80dd
jeshi2 <jamesvean19@gmail.com>
2023-05-12 22:57:16 +0300

    22:57

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Tuples addition"""
+# function that adds 2 tuples.
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    total_tuple = (a[0] + b[0], a[1] + b[1])
+    return total_tuple
