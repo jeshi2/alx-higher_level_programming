db988d64ec15017e63e537c4792733e508612cb1
jeshi2 <jamesvean19@gmail.com>
2023-06-01 09:45:07 +0300

    jeshi

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""square that set size as private instance variable"""
+
+
 class Square:
-    """ __size (int): The size of the square """
+    """ creation of square"""
+
     def __init__(self, size):
-        """Initializes a new instance of the Square class."""
+        """Initializes a new instance of the Square class.
+
+        Args:
+            size (int): the size of the square
+        """
         self.__size = size
