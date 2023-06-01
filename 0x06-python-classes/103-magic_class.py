fc0455eca08b8f5235d04e55705760cd9e68fddb
jeshi2 <jamesvean19@gmail.com>
2023-06-01 10:17:27 +0300

    jeshi

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
+"""interpreted a python class code bytecode"""
+
 import math
 
+
 class MagicClass:
+    """magiclass decode"""
+
     def __init__(self, radius=0):
+        """init class of magicclass"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
+        """calc area of the circle"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """calc the circumference"""
         return 2 * math.pi * self.__radius
