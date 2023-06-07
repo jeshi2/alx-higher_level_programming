37264a7755b550b78488028320507eb362253aa1
jeshi2 <jamesvean19@gmail.com>
2023-06-07 20:30:05 +0300

    jeshi

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""define class rectangle
+"""
+
+
+class Rectangle:
+    """rep a rec width height and width"""
+    def __init__(self, width=0, height=0):
+        """initial a rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """private instance attributes __width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set recta width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return area of a rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """return perimeter of a rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """return string
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(["#" * self.__width] * self.__height)
+
+    def __repr__(self):
+        """return representation
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
