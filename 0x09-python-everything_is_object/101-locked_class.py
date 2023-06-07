810af59dd83bc079ddcc38ac66dd564f2c0e5e2a
jeshi2 <jamesvean19@gmail.com>
2023-06-07 23:34:54 +0300

    jeshi

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,7 +2,7 @@
 class LockedClass:
     __slots__ = ['first_name']
 
-    def __setattr__(self, name, value):
+    """def __setattr__(self, name, value):
         if name != 'first_name':
             raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
-        self.__dict__[name] = value
+        self.__dict__[name] = value"""
