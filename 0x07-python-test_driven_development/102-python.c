fb63fba72e21845f0d5e183a7e7051235c7038ea
jeshi2 <jamesvean19@gmail.com>
2023-06-11 10:55:14 +0300

    jeshi

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,24 +1,16 @@
 #include <Python.h>
-#include <stdio.h>
-#include <string.h>
 
-/**
- * print_python_string - Prints info about python str
- * @p: pointer to PyObject
- * Return: error message if p is not valid
- **/
 void print_python_string(PyObject *p)
 {
-    PyUnicodeObject *str = (PyUnicodeObject *)p;
-
-    if (!PyUnicode_Check(str))
+    const cahr *value;
+    if (!PyUnicode_Check(p))
     {
         fprintf(stderr, "[ERROR] Invalid String Object\n");
         return;
     }
 
-    Py_ssize_t length = PyUnicode_GET_LENGTH(str);
-    Py_UCS4 *value = PyUnicode_AsUCS4Copy(str);
+    Py_ssize_t length = PyUnicode_GET_LENGTH(p);
+    value = PyUnicode_AsUTF8(p);
 
     if (!value)
     {
@@ -27,9 +19,7 @@ void print_python_string(PyObject *p)
     }
 
     printf("[.] string object info\n");
-    printf("  type: %s\n", PyUnicode_IS_COMPACT_ASCII(str) ? "compact ascii" : "compact unicode object");
+    printf("  type: compact %s\n", PyUnicode_IS_COMPACT_ASCII(p) ? "ascii" : "unicode object");
     printf("  length: %zd\n", length);
-    printf("  value: %ls\n", value);
-
-    PyMem_Free(value);
+    printf("  value: %s\n", value);
 }
