3e364a5cf61ac1bda85021b80f53d2741e4f74a9
jeshi2 <jamesvean19@gmail.com>
2023-06-11 11:02:31 +0300

    jeshi

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -8,25 +8,5 @@
  * Return: Nothing
  **/
 void print_python_string(PyObject *p)
-{
-    /*const cahr *value;
-    if (!PyUnicode_Check(p))
-    {
-        fprintf(stderr, "[ERROR] Invalid String Object\n");
-        return;
-    }
-
-    Py_ssize_t length = PyUnicode_GET_LENGTH(p);
-    value = PyUnicode_AsUTF8(p);
-
-    if (!value)
-    {
-        fprintf(stderr, "[ERROR] Invalid String Object\n");
-        return;
-    }
-
-    printf("[.] string object info\n");
-    printf("  type: compact %s\n", PyUnicode_IS_COMPACT_ASCII(p) ? "ascii" : "unicode object");
-    printf("  length: %zd\n", length);
-    printf("  value: %s\n", value);*/
+{  
 }
