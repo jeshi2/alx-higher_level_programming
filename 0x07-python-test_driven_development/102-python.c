bf9cc096708ab2786dd1f028b8efaf11eeef6e32
jeshi2 <jamesvean19@gmail.com>
2023-06-11 10:47:43 +0300

    jeshi

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,36 @@
+#include <Python.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_python_string - Prints info about python str
+ * @p: pointer to PyObject
+ * Return: Nothing
+ **/
+void print_python_string(PyObject *p)
+{
+    PyUnicodeObject *str = (PyUnicodeObject *)p;
+
+    if (!PyUnicode_Check(str))
+    {
+        fprintf(stderr, "[ERROR] Invalid String Object\n");
+        return;
+    }
+
+    Py_ssize_t length = PyUnicode_GET_LENGTH(str);
+    Py_UCS4 *value = PyUnicode_AsUCS4Copy(str);
+
+    if (!value)
+    {
+        fprintf(stderr, "[ERROR] Invalid String Object\n");
+        return;
+    }
+
+    printf("[.] string object info\n");
+    printf("  type: %s\n", PyUnicode_IS_COMPACT_ASCII(str) ? "compact ascii" : "compact unicode object");
+    printf("  length: %zd\n", length);
+    printf("  value: %ls\n", value);
+
+    PyMem_Free(value);
+}
+
