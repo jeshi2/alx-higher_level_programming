b44ac7374a0d473631c63ad7cdd3b1817f8b6fc9
jeshi2 <jamesvean19@gmail.com>
2023-06-20 09:47:39 +0300

    jeshi

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -114,3 +114,23 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.x, self.y, self.width, self.height
         )
+
+    def update(self, *args):
+        """
+        Assigns arguments to each attribute in the specified order:
+        1st argument -> id attribute
+        2nd argument -> width attribute
+        3rd argument -> height attribute
+        4th argument -> x attribute
+        5th argument -> y attribute
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
