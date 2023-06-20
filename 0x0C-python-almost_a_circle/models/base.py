35d7325e18f64aa9562626a816d3156a16f5a51e
jeshi2 <jamesvean19@gmail.com>
2023-06-20 08:31:26 +0300

    jeshi

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""define class base
+"""
+
+class Base:
+    """
+    Base class to manage id attribute.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Constructor for Base class.
+
+        Args:
+            id (int): Optional. The id value to assign.
+
+        Attributes:
+            id (int): The unique identifier of the instance.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import unittest
+from models.base import Base
+
+"""module test
+"""
+
+
+class TestBase(unittest.TestCase):
+    """
+    Test cases for the Base class.
+    """
+
+    def test_id_assignment(self):
+        """
+        Test id assignment when creating instances.
+        """
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+
+        b5 = Base()
+        self.assertEqual(b5.id, 4)
+
+if __name__ == '__main__':
+    unittest.main()
