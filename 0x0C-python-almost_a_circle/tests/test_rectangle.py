450c63b02bbb1d92c69b30ce5d19353d0960212e
jeshi2 <jamesvean19@gmail.com>
2023-06-21 00:14:13 +0300

    jeshi

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""test for rectangle with unittest"""
 import unittest
 from models.rectangle import Rectangle
 from models.base import Base
@@ -10,7 +11,8 @@ from contextlib import redirect_stdout
 
 
 class TestRectangle(unittest.TestCase):
-    def test_constructor(self):
+    """class for testing"""
+    def test_constructor_1(self):
         r = Rectangle(10, 5, 2, 3, 1)
         self.assertEqual(r.width, 10)
         self.assertEqual(r.height, 5)
@@ -103,6 +105,166 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r.width, 8)
         self.assertEqual(r.height, 6)
         self.assertEqual(r.x, 4)
+
+    def test_constructor_2(self):
+        rect = Rectangle(1, 2)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+
+    def test_constructor_3(self):
+        rect = Rectangle(1, 2, 3)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 3)
+
+    def test_constructor_4(self):
+        rect = Rectangle(1, 2, 3, 4)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 3)
+        self.assertEqual(rect.y, 4)
+
+    def test_constructor_5(self):
+        rect = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 3)
+        self.assertEqual(rect.y, 4)
+
+    def test_type(self):
+        self.assertRaises(TypeError, Rectangle, "1", 1)
+        self.assertRaises(TypeError, Rectangle, width='2')
+        self.assertRaises(TypeError, Rectangle, width=float('NaN'))
+        self.assertRaises(TypeError, Rectangle, width=float('inf'))
+        self.assertRaises(TypeError, Rectangle, 1, height='abc')
+        self.assertRaises(TypeError, Rectangle, 1, 1, x={})
+        self.assertRaises(TypeError, Rectangle, 1, 1, y=2.5)
+
+    def test_value(self):
+        self.assertRaises(ValueError, Rectangle, -5, 1)
+        self.assertRaises(ValueError, Rectangle, 0, 1)
+        self.assertRaises(ValueError, Rectangle, 1, 0)
+        self.assertRaises(ValueError, Rectangle, 1, -565)
+        self.assertRaises(ValueError, Rectangle, 1, 1, -755)
+        self.assertRaises(ValueError, Rectangle, 1, 1, -2)
+        self.assertRaises(ValueError, Rectangle, 1, 2, 3, -4)
+
+    def test_rectangle_representation(self):
+        rect_repr = str(Rectangle(1, 2, 3, 4, 5))
+        result = '[Rectangle] (5) 3/4 - 1/2'
+        self.assertEqual(rect_repr, result)
+
+    def test_rectangle_update_exists_1(self):
+        rect = Rectangle(5, 4, 3, 2, 1)
+        rect.update(89)
+        self.assertEqual(rect.id, 89)
+
+    def test_rectangle_update_exists_2(self):
+        rect = Rectangle(5, 4, 3, 2, 1)
+        rect.update(89, 1)
+        self.assertEqual(rect.id, 89)
+        self.assertEqual(rect.width, 1)
+
+    def test_rectangle_update_exists_3(self):
+        rect = Rectangle(5, 4, 3, 2, 1)
+        rect.update(89, 1, 2)
+        self.assertEqual(rect.id, 89)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+
+    def test_rectangle_update_exists_4(self):
+        rect = Rectangle(5, 4, 3, 2, 1)
+        rect.update(89, 1, 2, 3)
+        self.assertEqual(rect.id, 89)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 3)
+
+    def test_rectangle_update_exists_5(self):
+        rect = Rectangle(5, 4, 3, 2, 1)
+        rect.update(89, 1, 2, 3, 4)
+        self.assertEqual(rect.id, 89)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 3)
+        self.assertEqual(rect.y, 4)
+
+    def test_rectangle_update_exists_6(self):
+        rect = Rectangle(5, 4, 3, 2, 1)
+        rect.update(**{'id': 89})
+        self.assertEqual(rect.id, 89)
+
+    def test_rectangle_update_exists_7(self):
+        rect = Rectangle(5, 4, 3, 2, 1)
+        rect.update(**{'id': 89, 'width': 1})
+        self.assertEqual(rect.id, 89)
+        self.assertEqual(rect.width, 1)
+
+    def test_rectangle_update_exists_8(self):
+        rect = Rectangle(5, 4, 3, 2, 1)
+        rect.update(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual(rect.id, 89)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+
+    def test_rectangle_update_exists_9(self):
+        rect = Rectangle(5, 4, 3, 2, 1)
+        rect.update(**{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        self.assertEqual(rect.id, 89)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 3)
+
+    def test_rectangle_update_exists_10(self):
+        rect = Rectangle(5, 4, 3, 2, 1)
+        rect.update(**{'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+        self.assertEqual(rect.id, 89)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 3)
+        self.assertEqual(rect.y, 4)
+
+    def test_rectangle_create_exists_3(self):
+        rect = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual(rect.id, 89)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+
+    def test_rectangle_create_exists_4(self):
+        rect = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        self.assertEqual(rect.id, 89)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 3)
+
+    def test_rectangle_create_exists_5(self):
+        rect = Rectangle.create(**{
+            'id': 89,
+            'width': 1,
+            'height': 2,
+            'x': 3,
+            'y': 4
+        })
+        self.assertEqual(rect.id, 89)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 3)
+        self.assertEqual(rect.y, 4)
+
+    def test_basic_display(self):
+        r = Rectangle(2, 2)
+        input_string = io.StringIO()
+        res_str = "##\n##\n"
+        with redirect_stdout(input_string):
+            r.display()
+        self.assertEqual(res_str, input_string.getvalue())
+
+        r.x = 1
+        res_str = " ##\n ##\n"
+        input_string = io.StringIO()
+        with redirect_stdout(input_string):
+            r.display()
+        self.assertEqual(res_str, input_string.getvalue())
 
 if __name__ == '__main__':
     unittest.main()
