480319a88db10b0d0385db65a0cdfe89fcaa41a2
jeshi2 <jamesvean19@gmail.com>
2023-06-20 13:37:08 +0300

    jeshi

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """define class base
 """
+import json
 
 class Base:
     """
@@ -25,3 +26,52 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Converts a list of dictionaries to a JSON string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save a list of instances to a file in JSON format"""
+        filename = cls.__name__ + ".json"
+        json_list = []
+        if list_objs is not None:
+            for obj in list_objs:
+                json_list.append(obj.to_dictionary())
+        with open(filename, mode='w', encoding='utf-8') as file:
+            file.write(cls.to_json_string(json_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Converts a JSON string to a list of dictionaries"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Creates an instance from a dictionary"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Load instances from a file in JSON format"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, mode='r', encoding='utf-8') as file:
+                json_string = file.read()
+                json_list = cls.from_json_string(json_string)
+                instances = []
+                for item in json_list:
+                    instances.append(cls.create(**item))
+                return instances
+        except FileNotFoundError:
+            return []
