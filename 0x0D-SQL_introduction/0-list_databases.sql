6f1e46576bb82d2df123dfe6b9de62e3c4b6caf6
jeshi2 <jamesvean19@gmail.com>
2023-07-20 10:31:57 -0700

    int

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,2 @@
---script that lists all databases of Mysql server
+-- script that lists all databases of Mysql server
 SHOW DATABASES;
--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
---script that create a database in my sql server
---should not fail even if dbs exist
+-- script that create a database in my sql server
+-- should not fail even if dbs exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
