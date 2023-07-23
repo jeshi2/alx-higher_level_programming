35006115f74cfb740307f52196ce3e3469644127
jeshi2 <jamesvean19@gmail.com>
2023-07-23 09:10:36 +0300

    more_queries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,4 @@
 -- The states table contains only one record where name = California (but the id can be different, as per the example)
 -- Results must be sorted in ascending order by cities.id
 -- You are not allowed to use the JOIN keyword
-SELECT id FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+SELECT id FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id;
