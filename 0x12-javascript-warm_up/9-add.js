96c18f3e1999b452fef617d89136274701edb6ce
jeshi2 <jamesvean19@gmail.com>
2023-08-15 22:39:20 +0300

    mid

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+
+if (isNaN(num1) || isNaN(num2)) {
+  console.log('NaN');
+} else {
+  const output1 = add(num1, num2);
+  console.log(output1);
+}
