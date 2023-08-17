336450bdf788a1ea316e85f54867da37f31f73e9
jeshi2 <jamesvean19@gmail.com>
2023-08-17 10:09:08 +0300

    objects-scope-closure

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const RootSquare = require('./5-square');
+
+class Square extends RootSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
