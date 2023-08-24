dc56933781f4cce4ce396b1fb13ae96294a1bac0
jeshi2 <jamesvean19@gmail.com>
2023-08-24 12:18:26 +0300

    mid

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Script to retrieve and display states with names starting with 'N' from a MySQL database.
+"""
+
+import MySQLdb
+import sys
+
+def main():
+    """
+    Main function to connect to the database and retrieve filtered states.
+    """
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        cursor = db.cursor()
+
+        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
+        cursor.execute(query)
+        states = cursor.fetchall()
+
+        for state in states:
+            print(state)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+        sys.exit(1)
+
+    finally:
+        if db:
+            db.close()
+
+if __name__ == "__main__":
+    main()
