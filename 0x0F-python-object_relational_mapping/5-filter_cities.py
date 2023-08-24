5464f51d989f32ce7c546d6a6c531532f7bceae3
jeshi2 <jamesvean19@gmail.com>
2023-08-24 18:23:14 +0300

    mid

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+script list all cities of given state on db
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """
+    connect db and retrieve cities of the specified state
+    """
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        cursor = db.cursor()
+
+        query = """
+        SELECT GROUP_CONCAT(cities.name SEPARATOR ', ')
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """
+        cursor.execute(query, (state_name,))
+        cities = cursor.fetchone()[0]
+
+        if cities:
+            print(cities)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+        sys.exit(1)
+
+    finally:
+        if db:
+            db.close()
