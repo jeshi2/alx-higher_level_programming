2097903548465794b406abaafe98060e7c864309
jeshi2 <jamesvean19@gmail.com>
2023-08-26 21:20:24 +0300

    mid

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -13,10 +13,12 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     """connect db"""
     if len(sys.argv) != 4:
+        """ db connect """
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+            (sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
