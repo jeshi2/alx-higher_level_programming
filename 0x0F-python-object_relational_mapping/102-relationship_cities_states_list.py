8315cf3dd6a45096184b1e587d7b5a9d88a65ed8
jeshi2 <jamesvean19@gmail.com>
2023-08-26 21:52:59 +0300

    mid

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 script that lists all City objects from the database hbtn_0e_101_usa
 """
@@ -11,17 +10,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    connect db to retrive contecnt infomation
-    """
-
     if len(sys.argv) != 4:
-        """ db connect """
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
             (sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
