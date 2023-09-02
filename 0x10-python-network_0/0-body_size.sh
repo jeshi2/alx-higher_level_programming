75f1cad56a5be0d535e7ba83448a04bedd6485d6
jeshi2 <jamesvean19@gmail.com>
2023-09-02 18:46:23 +0300

    python_networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Bash script sends a request to a URL and displays the size of the response body in byte
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-size=$(curl -sI "$1" | grep -i Content-Length | awk '{print $2}')
-echo "$size"
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
