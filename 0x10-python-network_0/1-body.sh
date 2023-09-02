2a9e2df3ad5e5d6d7c1f620c71e2f5d5bdd23724
jeshi2 <jamesvean19@gmail.com>
2023-09-02 18:58:17 +0300

    python_networking

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sI "$1" | grep "HTTP/1.1 200 OK" && curl -s "$1" | tail -n +2
+curl -sL "$1"
