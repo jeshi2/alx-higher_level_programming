6a6410adc9612ad56af6a119b6996e11989cbc36
jeshi2 <jamesvean19@gmail.com>
2023-09-04 13:17:46 +0300

    python_networking#1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,49 +1,43 @@
 #!/usr/bin/python3
 """
-Python script that takes 2 arguments in order to
-solve hoberton challenge
+Python script that takes 2 arguments in
+order to solve this challenge.
 """
 import requests
 import sys
 
 
-def list_commits(owner, repo, count=10):
+def list_commits(owner, repo):
     """
-    Lists recent commits from a GitHub
-    repository by the specified owner
+    Lists 10 most recent commits of a repository by a user
     """
+    base_url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    params = {'per_page': 10}
+    headers = {'Accept': 'application/vnd.github.v3+json'}
+
     try:
-        url = (
-            f"https://api.github.com/repos/{owner}/{repo}/"
-            f"commits?per_page={count}"
-        )
-        response = requests.get(url)
+        response = requests.get(base_url, params=params, headers=headers)
 
         if response.status_code == 200:
             commits = response.json()
-            sorted_commits = sorted(
-                commits,
-                key=lambda x: x['commit']['author']['date'],
-                reverse=True)
-            for commit in sorted_commits:
+            for commit in commits:
                 sha = commit['sha']
                 author_name = commit['commit']['author']['name']
                 print(f"{sha}: {author_name}")
         else:
-            print(
-                f"Error: Unable to fetch commits "
-                f"(HTTP {response.status_code})"
-            )
+            print(f"Error: {response.status_code}")
     except Exception as e:
-        print(f"Error: {e}")
+        print(e)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python3 100-github_commits.py <owner> <repo>")
+        print(f"Usage: python3 100 - github_commits.py "
+              f"< repository_name > <owner_name >"
+              )
         sys.exit(1)
 
-    owner = sys.argv[1]
-    repo = sys.argv[2]
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
 
-    list_commits(owner, repo)
+    list_commits(owner_name, repo_name)
