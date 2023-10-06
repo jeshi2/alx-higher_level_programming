81b16f5b76b229ae85ae72dd0aa8bb80fc47ca4e
jeshi2 <jamesvean19@gmail.com>
2023-10-06 19:59:36 +0300

    webscrapping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -11,24 +11,59 @@ if (process.argv.length !== 3) {
 // Get the API URL from the command line argument
 const apiUrl = process.argv[2];
 
-// Make a GET request to the specified API URL
-request(apiUrl, (error, response, body) => {
+// Function to make a GET request to the API URL
+function makeRequest(url, callback) {
+  request(url, (error, response, body) => {
+    if (error) {
+      // If an error occurs during the request, call the callback with an error
+      callback(error);
+    } else if (response.statusCode !== 200) {
+      // If the response status code is not 200, call the callback with an error
+      callback(`Error: Unable to retrieve movie details. Status code: ${response.statusCode}`);
+    } else {
+      // Parse the JSON response
+      const data = JSON.parse(body);
+
+      // Call the callback with the parsed data
+      callback(null, data);
+    }
+  });
+}
+
+// Function to count movies with Wedge Antilles
+function countMoviesWithWedge(apiUrl, callback, moviesWithWedge = []) {
+  makeRequest(apiUrl, (error, data) => {
+    if (error) {
+      // If there is an error, call the callback with the error
+      callback(error);
+    } else {
+      // Filter movies where "Wedge Antilles" (character ID 18) is present
+      const movies = data.results.filter((movie) =>
+        movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+      );
+
+      // Add filtered movies to the array
+      moviesWithWedge = moviesWithWedge.concat(movies);
+
+      // Check if there are more pages of results
+      if (data.next) {
+        // If there are more pages, recursively call the function with the next URL
+        countMoviesWithWedge(data.next, callback, moviesWithWedge);
+      } else {
+        // If there are no more pages, call the callback with the count of movies
+        callback(null, moviesWithWedge.length);
+      }
+    }
+  });
+}
+
+// Call the countMoviesWithWedge function to get the count of movies with Wedge Antilles
+countMoviesWithWedge(apiUrl, (error, count) => {
   if (error) {
-    // If an error occurs during the request, print the error
+    // If there is an error, print the error message
     console.error(error);
-  } else if (response.statusCode !== 200) {
-    // If the response status code is not 200, print an error message
-    console.error(`Error: Unable to retrieve movie details. Status code: ${response.statusCode}`);
   } else {
-    // Parse the JSON response
-    const data = JSON.parse(body);
-
-    // Filter movies where "Wedge Antilles" (character ID 18) is present
-    const moviesWithWedge = data.results.filter((movie) =>
-      movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-    );
-
-    // Print the number of movies where Wedge Antilles is present
-    console.log(moviesWithWedge.length);
+    // Print the count of movies with Wedge Antilles
+    console.log(count);
   }
 });
