cea1153e71f77da0a3991131129609dd84af1b61
jeshi2 <jamesvean19@gmail.com>
2023-10-07 21:54:31 +0300

    With JQuery

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Wait for the document to be ready
+$(document).ready(function() {
+  // Select the <header> element using jQuery and update its text color to red
+  $('header').css('color', '#FF0000');
+});
