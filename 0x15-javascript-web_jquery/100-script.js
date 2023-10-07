3416668e75d25058afbe0c00dffed561fd6e96bc
jeshi2 <jamesvean19@gmail.com>
2023-10-07 22:28:31 +0300

    No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+// Wait for the document to be ready
+document.addEventListener('DOMContentLoaded', function() {
+  // Select the <header> element using document.querySelector
+  const headerElement = document.querySelector('header');
+
+  // Update the text color to red (#FF0000)
+  headerElement.style.color = '#FF0000';
+});
